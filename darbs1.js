cda8d21a9f8342535d306606ff1dc3759e8275de
Baiba1 <baiba.balode@grobinasskola.lv>
2021-09-28 14:32:25 +0300

    NOSŪTU SAVU DARBU

---
+++ darbs1.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    document.getElementById("poga").innerHTML = "Sveiki!";}
+
+
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(95,50,40,0,2*Math.PI);
+  ctx.stroke();
